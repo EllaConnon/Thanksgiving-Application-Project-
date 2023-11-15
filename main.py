97f073e2013153a39aa412957d4d39fa000e535e
EllaConnon <ec122408@wgcloud.org>
2023-11-15 11:10:35 -0600

    added Resipy Array and RT varible

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 #Thanksgiving Recipe Organizer
+#Welcome Messages
+
+#Adding in Resipes
+while True:
+   Recipe = []
+   recipeTitle = input("What is the title/name of the recipe? ")
+   Recipe.append({
+       "Recipe Title": recipeTitle
+   })
