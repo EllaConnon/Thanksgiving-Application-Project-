6c6f1053ff728167ce40b1d961dc560e548b2afb
EllaConnon <ec122408@wgcloud.org>
2023-11-15 11:20:00 -0600

    added # of resRecipe

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-#Thanksgiving Recipe Organizer
+#Thanksgiving Recipe Organizer Allowing for 5 Resipes
 #Welcome Messages
-
+#Define Array
+Recipe = []
 #Adding in Resipes
 while True:
-   Recipe = []
+   # Adding in Recipe Title
    recipeTitle = input("What is the title/name of the recipe? ")
    Recipe.append({
        "Recipe Title": recipeTitle
