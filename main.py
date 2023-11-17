fb3c1959d3df00747992a3042be0f41b1d772197
EllaConnon <ec122408@wgcloud.org>
2023-11-17 10:59:57 -0600

    fixed indentation

--- main.py
+++ main.py
@@ -6,22 +6,22 @@ Recipe = []
 #Adding in Resipes
 while True:
     numberOfRecipes = int(input("How many Resipies are you adding? (1-5) "))
-    for i in range(numberOfRecipes):
+    for i in range(1, numberOfRecipes+1, 1):
         # Adding in Recipe
         recipeTitle = input("What is the title/name of the recipe? ")
         Recipe.append({
             "Recipe Title": recipeTitle
         })
-    if numberOfRecipes == 1:
-        RecipesOne = []
-    elif numberOfRecipes == 2:
-        RecipesTwo = []
-    elif numberOfRecipes == 3:
-        RecipesThree = []
-    elif numberOfRecipes == 4:
-        RecipesFour = []
-    elif numberOfRecipes == 5:
-        RecipesFive = []
-    else:
-        print("Error! Pick a number 1 through 5.")
-        continue
+        if i == 1:
+            RecipesOne = []
+        elif i == 2:
+            RecipesTwo = []
+        elif i == 3:
+            RecipesThree = []
+        elif i == 4:
+            RecipesFour = []
+        elif i == 5:
+            RecipesFive = []
+        else:
+            print("Error! Pick a number 1 through 5.")
+            continue
