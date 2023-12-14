3c6b5c9d2908bc1b121a36c728d6ddd70898cd80
purpleTree2 <137818802+purpleTree2@users.noreply.github.com>
2023-12-14 15:58:24 -0600

    finaly fixed

--- main.py
+++ main.py
@@ -35,169 +35,169 @@ while True:
         recipeTitle = input("What is the title/name of the recipe? ")
         Recipe.append(recipeTitle)
 #Recipe 1
-        if i == 1:
-           prepTime = int(input("How many minutes does it take to prep for this recipe? "))
-           cookTime = int(input("How many minutes does it take to cook this recipe? "))
-           servings = float(input("How many servings does the recipe yield? "))
-           RecipesOne.append({
+        if i == 1 :
+            prepTime = input("How many minutes does it take to prep for this recipe? ")
+            cookTime = int(input("How many minutes does it take to cook this recipe? "))
+            servings = float(input("How many servings does the recipe yield? "))
+            RecipesOne.append({
                "Prep Time(minutes): ": prepTime
-           })
-           RecipesOne.append({
-               "Cook Time(minutes): ": cookTime
-           })
-           RecipesOne.append({
-               "Yields: ": servings
-           })
-           numberOfIngredients = int(input("How many ingredients are in the recipe? "))
-           for w in range(numberOfIngredients):
-               ingredient = input("What is the ingredient? ")
-               unit = input("What unit is the measurement in? ")
-               amount = float(input("How much of " + str(ingredient) + "? "))
-               ingredientsOne.append(ingredient)
-               ingredientsOne.append(amount)
-               ingredientsOne.append(unit)
-           RecipesOne.append(ingredientsOne)
-           while steps:
-               print("Just enter the step, no numbers in front of the step.")
-               step = input("What is the step? ")
-               stepsOne.append(step)
-               steps = input("Are there more steps? ")
-               steps = steps == "yes" or steps == "Yes"
-           RecipesOne.append(stepsOne)
-           Recipe.append(RecipesOne)
-           print(" ")
+            })
+            RecipesOne.append({
+                "Cook Time(minutes): ": cookTime
+            })
+            RecipesOne.append({
+                "Yields: ": servings
+            })
+            numberOfIngredients = int(input("How many ingredients are in the recipe? "))
+            for w in range(numberOfIngredients):
+                ingredient = input("What is the ingredient? ")
+                unit = input("What unit is the measurement in? ")
+                amount = float(input("How much of " + str(ingredient) + "? "))
+                ingredientsOne.append(ingredient)
+                ingredientsOne.append(amount)
+                ingredientsOne.append(unit)
+            RecipesOne.append(ingredientsOne)
+            while steps:
+                print("Just enter the step, no numbers in front of the step.")
+                step = input("What is the step? ")
+                stepsOne.append(step)
+                steps = input("Are there more steps? ")
+                steps = steps == "yes" or steps == "Yes"
+            RecipesOne.append(stepsOne)
+            Recipe.append(RecipesOne)
+            print(" ")
 #Recipe 2
         elif i == 2:
-           prepTime = int(input("How many minutes does it take to prep for this recipe? "))
-           cookTime = int(input("How many minutes does it take to cook this recipe? "))
-           servings = float(input("How many servings does the recipe yield? "))
-           RecipesTwo.append({
-               "Prep Time(minutes): ": prepTime
-           })
-           RecipesTwo.append({
-               "Cook Time(minutes): ": cookTime
-           })
-           RecipesTwo.append({
+            prepTime = int(input("How many minutes does it take to prep for this recipe? "))
+            cookTime = int(input("How many minutes does it take to cook this recipe? "))
+            servings = float(input("How many servings does the recipe yield? "))
+            RecipesTwo.append({
+                "Prep Time(minutes): ": prepTime
+            })
+            RecipesTwo.append({
+                "Cook Time(minutes): ": cookTime
+            })
+            RecipesTwo.append({
                "Yields: ": servings
-           })
-           numberOfIngredients = int(input("How many ingredients are in the recipe? "))
-           for w in range(numberOfIngredients):
+            })
+            numberOfIngredients = int(input("How many ingredients are in the recipe? "))
+            for w in range(numberOfIngredients):
                ingredient = input("What is the ingredient? ")
                unit = input("What unit is the measurement in? ")
                amount = float(input("How much of " + str(ingredient) + "? "))
                ingredientsTwo.append(ingredient)
                ingredientsTwo.append(amount)
                ingredientsTwo.append(unit)
-           RecipesTwo.append(ingredientsTwo)
-           while steps:
+            RecipesTwo.append(ingredientsTwo)
+            while steps:
                print("Just enter the step, no numbers in front of the step.")
                step = input("What is the step? ")
                stepTwo.append(step)
                steps = input("Are there more steps? ")
                steps = steps == "yes" or steps == "Yes"
-           RecipesTwo.append(stepTwo)
-           Recipe.append(RecipesTwo)
-           print(" ")
+            RecipesTwo.append(stepTwo)
+            Recipe.append(RecipesTwo)
+            print(" ")
 #Recipe 3
         elif i == 3:
-           prepTime = int(input("How many minutes does it take to prep for this recipe? "))
-           cookTime = int(input("How many minutes does it take to cook this recipe? "))
-           servings = float(input("How many servings does the recipe yield? "))
-           RecipesThree.append({
-               "Prep Time(minutes): ": prepTime
-           })
-           RecipesThree.append({
-               "Cook Time(minutes): ": cookTime
-           })
-           RecipesThree.append({
-               "Yields: ": servings
-           })
-           numberOfIngredients = int(input("How many ingredients are in the recipe? "))
-           for w in range(numberOfIngredients):
-               ingredient = input("What is the ingredient? ")
-               unit = input("What unit is the measurement in? ")
-               amount = float(input("How much of " + str(ingredient) + "? "))
-               ingredientsThree.append(ingredient)
-               ingredientsThree.append(amount)
-               ingredientsThree.append(unit)
-           RecipesThree.append(ingredientsThree)
-           while steps:
-               print("Just enter the step, no numbers in front of the step.")
-               step = input("What is the step? ")
-               stepThree.append(step)
-               steps = input("Are there more steps? ")
-               steps = steps == "yes" or steps == "Yes"
-           RecipesThree.append(stepThree)
-           Recipe.append(RecipesThree)
-           print(" ")
+            prepTime = int(input("How many minutes does it take to prep for this recipe? "))
+            cookTime = int(input("How many minutes does it take to cook this recipe? "))
+            servings = float(input("How many servings does the recipe yield? "))
+            RecipesThree.append({
+                "Prep Time(minutes): ": prepTime
+            })
+            RecipesThree.append({
+                "Cook Time(minutes): ": cookTime
+            })
+            RecipesThree.append({
+                "Yields: ": servings
+            })
+            numberOfIngredients = int(input("How many ingredients are in the recipe? "))
+            for w in range(numberOfIngredients):
+                ingredient = input("What is the ingredient? ")
+                unit = input("What unit is the measurement in? ")
+                amount = float(input("How much of " + str(ingredient) + "? "))
+                ingredientsThree.append(ingredient)
+                ingredientsThree.append(amount)
+                ingredientsThree.append(unit)
+            RecipesThree.append(ingredientsThree)
+            while steps:
+                print("Just enter the step, no numbers in front of the step.")
+                step = input("What is the step? ")
+                stepThree.append(step)
+                steps = input("Are there more steps? ")
+                steps = steps == "yes" or steps == "Yes"
+            RecipesThree.append(stepThree)
+            Recipe.append(RecipesThree)
+            print(" ")
 #Recipe 4
         elif i == 4:
-           prepTime = int(input("How many minutes does it take to prep for this recipe? "))
-           cookTime = int(input("How many minutes does it take to cook this recipe? "))
-           servings = float(input("How many servings does the recipe yield? "))
-           RecipesFour.append({
-               "Prep Time(minutes): ": prepTime
-           })
-           RecipesFour.append({
-               "Cook Time(minutes): ": cookTime
-           })
-           RecipesFour.append({
-               "Yields: ": servings
-           })
-           numberOfIngredients = int(input("How many ingredients are in the recipe? "))
-           for w in range(numberOfIngredients):
-               ingredient = input("What is the ingredient? ")
-               unit = input("What unit is the measurement in? ")
-               amount = float(input("How much of " + str(ingredient) + "? "))
-               ingredientsFour.append(ingredient)
-               ingredientsFour.append(amount)
-               ingredientsFour.append(unit)
-           RecipesFour.append(ingredientsFour)
-           while steps:
-               print("Just enter the step, no numbers in front of the step.")
-               step = input("What is the step? ")
-               stepFour.append(step)
-               steps = input("Are there more steps? ")
-               steps = steps == "yes" or steps == "Yes"
-           RecipesFour.append(stepFour)
-           Recipe.append(RecipesFour)
-           print(" ")
+            prepTime = int(input("How many minutes does it take to prep for this recipe? "))
+            cookTime = int(input("How many minutes does it take to cook this recipe? "))
+            servings = float(input("How many servings does the recipe yield? "))
+            RecipesFour.append({
+                "Prep Time(minutes): ": prepTime
+            })
+            RecipesFour.append({
+                "Cook Time(minutes): ": cookTime
+            })
+            RecipesFour.append({
+                "Yields: ": servings
+            })
+            numberOfIngredients = int(input("How many ingredients are in the recipe? "))
+            for w in range(numberOfIngredients):
+                ingredient = input("What is the ingredient? ")
+                unit = input("What unit is the measurement in? ")
+                amount = float(input("How much of " + str(ingredient) + "? "))
+                ingredientsFour.append(ingredient)
+                ingredientsFour.append(amount)
+                ingredientsFour.append(unit)
+            RecipesFour.append(ingredientsFour)
+            while steps:
+                print("Just enter the step, no numbers in front of the step.")
+                step = input("What is the step? ")
+                stepFour.append(step)
+                steps = input("Are there more steps? ")
+                steps = steps == "yes" or steps == "Yes"
+            RecipesFour.append(stepFour)
+            Recipe.append(RecipesFour)
+            print(" ")
 #Recipe 5
         elif i == 5:
-           prepTime = int(input("How many minutes does it take to prep for this recipe? "))
-           cookTime = int(input("How many minutes does it take to cook this recipe? "))
-           servings = float(input("How many servings does the recipe yield? "))
-           RecipesFive.append({
-               "Prep Time in minutes: ": prepTime
-           })
-           RecipesFive.append({
-               "Cook Time in minutes: ": cookTime
-           })
-           RecipesFive.append({
-               "Yields: ": servings
-           })
-           numberOfIngredients = int(input("How many ingredients are in the recipe? "))
-           for w in range(numberOfIngredients):
-               ingredient = input("What is the ingredient? ")
-               unit = input("What unit is the measurement in? ")
-               amount = float(input("How much of " + str(ingredient) + "? "))
-               ingredientsFive.append(ingredient)
-               ingredientsFive.append(amount)
-               ingredientsFive.append(unit)
-           RecipesFive.append(ingredientsFive)
-           while steps:
-               print("Just enter the step, no numbers in front of the step.")
-               step = input("What is the step? ")
-               stepFive.append(step)
-               steps = input("Are there more steps? ")
-               steps = steps == "yes" or steps == "Yes"
-           RecipesFive.append(stepFive)
-           Recipe.append(RecipesFive)
-           print(" ")
+            prepTime = int(input("How many minutes does it take to prep for this recipe? "))
+            cookTime = int(input("How many minutes does it take to cook this recipe? "))
+            servings = float(input("How many servings does the recipe yield? "))
+            RecipesFive.append({
+                "Prep Time in minutes: ": prepTime
+            })
+            RecipesFive.append({
+                "Cook Time in minutes: ": cookTime
+            })
+            RecipesFive.append({
+                "Yields: ": servings
+            })
+            numberOfIngredients = int(input("How many ingredients are in the recipe? "))
+            for w in range(numberOfIngredients):
+                ingredient = input("What is the ingredient? ")
+                unit = input("What unit is the measurement in? ")
+                amount = float(input("How much of " + str(ingredient) + "? "))
+                ingredientsFive.append(ingredient)
+                ingredientsFive.append(amount)
+                ingredientsFive.append(unit)
+            RecipesFive.append(ingredientsFive)
+            while steps:
+                print("Just enter the step, no numbers in front of the step.")
+                step = input("What is the step? ")
+                stepFive.append(step)
+                steps = input("Are there more steps? ")
+                steps = steps == "yes" or steps == "Yes"
+            RecipesFive.append(stepFive)
+            Recipe.append(RecipesFive)
+            print(" ")
 #else error statment
         else:
-           print("Error! Pick a number 1 through 5.")
-           continue
+            print("Error! Pick a number 1 through 5.")
+            continue
     break
 while True:
     num = len(Recipe)
@@ -290,9 +290,10 @@ while True:
         for q in range(len(stepFive)):
             print(str(m) +".) "+ str(stepFive[q]))
             m = m+1
-    
+    print(" ")
     ask = input("Would you like to view another recipe? ")
     if ask == "yes" or ask == "Yes":
         continue
     else:
         break
+print("Thank you for using this program!")
